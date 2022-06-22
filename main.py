b5675d0aa9bc47e40067beee1150ea8a8d1bfe47
hadi tayanloo <htayanloo@gmail.com>
2022-06-22 19:12:23 +0430

    fix

--- main.py
+++ main.py
@@ -33,25 +33,29 @@ def dispach_ports(ports):
     return result
 
 
-def action(src_ip,dst_ip,port,action,protocol,final_result):
-        final_result.append(f"{action} {protocol} host {src_ip} host {dst_ip} eq {port}")
-        final_result.append(f"{action} {protocol} host {dst_ip} eq {port} host {src_ip} ")
+def action(src_ip,dst_ip,port,action,protocol,final_result_src_to_dst,final_result_dst_to_src):
+        final_result_src_to_dst.append(f"{action} {protocol} host {src_ip} host {dst_ip} eq {port}")
+        final_result_dst_to_src.append(f"{action} {protocol} host {dst_ip} eq {port} host {src_ip} ")
 
 
 
-
+final_result_src_to_dst =[]
+final_result_dst_to_src =[]
 
 df = pd.read_excel (r'input.xlsx')
-final_result =[]
+
 for index, row in df[0:].iterrows():
     for src_ip in dispach_address(row[0]):
         for dst_ip in dispach_address(row[1]):
             for port in dispach_ports(row[2]):
-                action(src_ip,dst_ip,port,row[3],row[4],final_result)
+                action(src_ip,dst_ip,port,row[3],row[4],final_result_src_to_dst,final_result_dst_to_src)
 
 
 with open("result.txt","w+", encoding = 'utf-8') as f:
-    for item in final_result:
+    for item in final_result_src_to_dst:
+        # write each item on a new line
+        f.write("%s\n" % item)
+    for item in final_result_dst_to_src:
         # write each item on a new line
         f.write("%s\n" % item)
 
